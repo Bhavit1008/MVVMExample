fb14b2b75b730f846eb04c610f9e3b47d68aa8f2
GIGA <bhavitkanthalia16@gmail.com>
2021-01-04 10:28:51 +0530

    network and retrofit setup

--- app/src/main/java/com/example/mvvmexample/viewmodel/LoginViewModel.kt
+++ app/src/main/java/com/example/mvvmexample/viewmodel/LoginViewModel.kt
@@ -1,8 +1,24 @@
 package com.example.mvvmexample.viewmodel
 
+import android.view.View
 import androidx.lifecycle.ViewModel
+import com.example.mvvmexample.view.auth.LoginListner
 
 class LoginViewModel:ViewModel() {
 
+    var email:String = ""
+    var password:String = ""
+
+    var loginListner:LoginListner? = null
+
+    fun onLoginButtonClick(view:View){
+        if(email.isNullOrEmpty() || password.isNullOrEmpty()){
+            loginListner?.onFailure("please enter mail and password is mendatory")
+            return
+        }
+        else{
+
+        }
+    }
 
 }
